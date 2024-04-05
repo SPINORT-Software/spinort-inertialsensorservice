a7ac5b4644466b0ce29a2a42be45996b0a3042d8
Sharan Chitta <chitta.sharan@gmail.com>
2024-04-05 18:11:51 -0400

    clear

--- assemblers/kafka_assembler.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from kafka.consumer.fetcher import ConsumerRecord
-
-
-class KafkaAssembler(ABC):
-    @abstractmethod
-    def assemble(self, kafka_message: ConsumerRecord):
-        pass
